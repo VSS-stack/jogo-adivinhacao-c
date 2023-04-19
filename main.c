ac9b33a86019678d502fd9f3292f5000cf9f5faa
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-04-18 21:46:45 -0300

    melhorando tratamento dos valores e gerando números aleatórios

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
 
 int main() {
     //imprime o cabeçalho do jogo
@@ -8,7 +11,9 @@ int main() {
     printf("* Bem-vindo ao nosso jogo de adivinhação! *\n");
     printf("*******************************************\n");
 
-    int numerosecreto = 42;
+    srand(time(0)); //definindo 'semente' aleatória para a função rand()
+
+    int numerosecreto = rand() % 100;
     int chute;
     int tentativas = 1;
     int ganhou = 0;
@@ -38,20 +43,21 @@ int main() {
         }else if(maior) {
             printf("Seu chute foi maior que o número secreto.\n\n");
             printf("*******************************************\n");
-            pontosperdidos = (chute - numerosecreto) / 2.0;
         }else {
             printf("Seu chute foi menor que o número secreto.\n\n");
             printf("*******************************************\n");
-            pontosperdidos = (numerosecreto - chute) / 2.0;
         }
 
         tentativas++;
+        pontosperdidos = abs(chute - numerosecreto) / (float)2;
         pontos = pontos - pontosperdidos;
     }
 
     printf("\nFim de jogo!\n");
     printf("\nVocê acertou em %d tentativas!\n", tentativas);
-    printf("\nTotal de pontos: %.2f / 1000\n", pontos);
+    printf("\nTotal de pontos: %.1f / 1000\n", pontos);
+    printf("\nPressione qualquer tecla para encerrar o programa.\n");
 
+    getch();
     return 0;
 }
